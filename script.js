b7be490449848bd04f1f30df06901b8231ab0f28
SergeySergeychev <ssergeychev@inbox.lv>
2022-05-09 21:48:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element ,then play
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    console.log(mediaStream);
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+    console.log(videoElement);
+  } catch (error) {
+    // Catch Error Here
+    console.log(error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Pucture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
